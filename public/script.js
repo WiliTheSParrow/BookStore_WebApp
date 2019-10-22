41cac69d461282f25a9e130664cf4cb68dcc1ba8
WiliTheSparrow <w.julia91@gmail.com>
2019-10-22 16:57:25 +0200

    fetch data

--- public/script.js
+++ public/script.js
@@ -1,8 +1,22 @@
-var app = new Vue ({
+var app = new Vue({
     el: "#app",
-    data:{},
-    created:function(){
-        console.log("I'm running from Vue.");
+    data: {
+        allbooks: []
     },
-    methods: {}
+    created: function () {
+        this.getData("https://api.myjson.com/bins/zyv02");
+    },
+    methods: {
+        getData: function (link) {
+            fetch(link, {
+                    method: "GET",
+                })
+                .then(response => response.json())
+                .then(json => {
+                    data = json;
+                    app.allbooks = data.books;
+                })
+                .catch(error => error);
+        }
+    }
 })
