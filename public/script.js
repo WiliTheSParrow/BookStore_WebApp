f18d9cf5d44fa9114b15b4a76b61ffe6bc5c6a58
WiliTheSparrow <w.julia91@gmail.com>
2019-10-23 17:56:01 +0200

    bookfilter works

--- public/script.js
+++ public/script.js
@@ -1,7 +1,11 @@
 var app = new Vue({
     el: "#app",
     data: {
-        allbooks: []
+        allbooks: [],
+        filteredBooks: [],
+        searchKey: "",
+        hideTable: false,
+        hideNoMatch: true
     },
     created: function () {
         this.getData("https://api.myjson.com/bins/14f61w");
@@ -18,5 +22,21 @@ var app = new Vue({
                 })
                 .catch(error => error);
         }
-    }
+    },
+
+    computed: {
+        filterBooks: function () {
+            this.filteredBooks = this.allbooks.filter((allbooks) => {
+                return allbooks.title.toLowerCase().match(this.searchKey.toLowerCase()) || allbooks.description.toLowerCase().match(this.searchKey.toLowerCase()) || allbooks.language.toLowerCase().match(this.searchKey.toLowerCase());
+            });
+            if (this.filteredBooks == "") {
+                this.hideTable = true;
+                this.hideNoMatch = false;
+            } else {
+                this.hideTable = false;
+                this.hideNoMatch = true;
+            }
+            return this.filteredBooks;
+        }
+    },
 })
